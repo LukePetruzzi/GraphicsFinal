6bca03f7ba653b55ffaa49e31277b840d354d582
Luke Petruzzi <lukepetruzzi@gmail.com>
2018-05-11 16:24:42 -0400

    broken pitch and frequency game

--- sketch.js
+++ sketch.js
@@ -6,11 +6,15 @@
 const LEVEL_ZOOMED_OUT = 300;
 const CAM_SPEED = 20;
 
-const HZ_LOW = 100;
+const HZ_LOW = 230;
 const HZ_HI = 500;
 
 const SPECTRUM_BAND_TOP = 70;
 const SPECTRUM_BAND_BOTTOM = 15;
+
+var buf = new Float32Array(1024);
+var MIN_SAMPLES = 0;
+var GOOD_ENOUGH_CORRELATION = 0.9;
 
 var mic;
 var fft;
@@ -21,6 +25,8 @@ let lastCamPosY = 0
 
 let circleWidth = 50;
 let circleHeight = 50;
+let circleFrequency = 0;
+let circleVolume = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -54,9 +60,6 @@ function draw() {
 
     let fundamentalFreq = getFundamentalFrequency(spectrum);
 
-    ellipse(width / 2, constrain(height - micLevel * height * 5, 0, height), circleWidth, circleHeight);
-
-
     var waveform = fft.waveform();
     noFill();
     beginShape();
@@ -68,6 +71,17 @@ function draw() {
         vertex(x, y)
     }
     endShape();
+
+    let freq = autoCorrelate(waveform, sampleRate());
+    // console.log("freq: ", freq);
+    if (freq != -1 && freq > HZ_LOW && freq < HZ_HI) {
+        circleFrequency = map(freq, HZ_LOW, HZ_HI, 0, width);
+        circleVolume = constrain(micLevel * height * 5, 0, height);
+
+    }
+    fill(255, 255, 255);
+    ellipse(circleFrequency, circleVolume, circleWidth, circleHeight);
+
 }
 function normalize(val, min, max) {
     return (val - min) / (max - min);
@@ -77,7 +91,62 @@ function getFundamentalFrequency(spectrum) {
     let loudest = -1;
 
 }
+function autoCorrelate(buf, sampleRate) {
+    var SIZE = buf.length;
+    var MAX_SAMPLES = Math.floor(SIZE / 2);
+    var best_offset = -1;
+    var best_correlation = 0;
+    var rms = 0;
+    var foundGoodCorrelation = false;
+    var correlations = new Array(MAX_SAMPLES);
 
+    for (var i = 0; i < SIZE; i++) {
+        var val = buf[i];
+        rms += val * val;
+    }
+    rms = Math.sqrt(rms / SIZE);
+    if (rms < 0.01) { // not enough signal
+        // console.log("NOT ENOUGH SIGNAL");
+        return -1;
+    }
+
+    var lastCorrelation = 1;
+    for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
+        var correlation = 0;
+
+        for (var i = 0; i < MAX_SAMPLES; i++) {
+            correlation += Math.abs((buf[i]) - (buf[i + offset]));
+        }
+        correlation = 1 - (correlation / MAX_SAMPLES);
+        correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+        if ((correlation > GOOD_ENOUGH_CORRELATION) && (correlation > lastCorrelation)) {
+            foundGoodCorrelation = true;
+            if (correlation > best_correlation) {
+                best_correlation = correlation;
+                best_offset = offset;
+            }
+        } else if (foundGoodCorrelation) {
+            // short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
+            // Now we need to tweak the offset - by interpolating between the values to the left and right of the
+            // best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
+            // we need to do a curve fit on correlations[] around best_offset in order to better determine precise
+            // (anti-aliased) offset.
+
+            // we know best_offset >=1,
+            // since foundGoodCorrelation cannot go to true until the second pass (offset=1), and
+            // we can't drop into this clause until the following pass (else if).
+            var shift = (correlations[best_offset + 1] - correlations[best_offset - 1]) / correlations[best_offset];
+            return sampleRate / (best_offset + (8 * shift));
+        }
+        lastCorrelation = correlation;
+    }
+    if (best_correlation > 0.5) {
+        // console.log("f = " + sampleRate / best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+        return sampleRate / best_offset;
+    }
+    return -1;
+    //	var best_frequency = sampleRate/best_offset;
+}
 // function cameraPosition() {
 //     // // only change camera position if the mouse is pressed
 //     let posChangeX = 0;
