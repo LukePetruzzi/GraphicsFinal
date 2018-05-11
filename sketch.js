34cd9778cfc6d14c9d65dbd9af691286cd51fa8a
Luke Petruzzi <lukepetruzzi@gmail.com>
2018-05-11 13:06:32 -0400

    first tries

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+// const p5 = new p5();
+
+var mic;
 function setup() {
-    console.log("HELLO!!");
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    mic = new p5.AudioIn()
+    mic.start();
 }
-
 function draw() {
+    background(0);
 
+    micLevel = mic.getLevel();
+    ellipse(0, constrain(height - micLevel * height * 5, 0, height), 10, 10);
 }
